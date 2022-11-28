5fd58a9ff5b8f51e1ecde28c3560ef97483c62a2
lexshpin <lexshpin@gmail.com>
2022-11-28 11:08:41 -0500

    Implementing Security

--- src/main/java/com/lexshpin/PersonalFinances/security/SecurityConfig.java
+++ src/main/java/com/lexshpin/PersonalFinances/security/SecurityConfig.java
@@ -1,4 +1,55 @@
 package com.lexshpin.PersonalFinances.security;
 
+import com.lexshpin.PersonalFinances.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeRequests().requestMatchers("/auth/login", "auth/registration", "/error").permitAll()
+                .anyRequest().hasRole("USER")
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/dashboard")
+                .failureUrl("/auth/login?error")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/login");
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        authenticationProvider().setUserDetailsService(userService);
+        authenticationProvider().setPasswordEncoder(getPasswordEncoder());
+
+        return authenticationProvider();
+    }
 }
--- src/main/java/com/lexshpin/PersonalFinances/service/UserService.java
+++ src/main/java/com/lexshpin/PersonalFinances/service/UserService.java
@@ -3,12 +3,15 @@ package com.lexshpin.PersonalFinances.service;
 import com.lexshpin.PersonalFinances.model.User;
 import com.lexshpin.PersonalFinances.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepo userRepo;
 
@@ -41,5 +44,10 @@ public class UserService {
 
     public void delete(int id) {
         userRepo.deleteById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
